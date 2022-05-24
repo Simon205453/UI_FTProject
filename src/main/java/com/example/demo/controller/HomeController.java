58d142ed92e609d15c20054b017aa6f94cd68d4e
Simon <simonffsss@gmail.com>
2022-05-25 00:19:47 +0200

    et commmit

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,13 +1,19 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/index")
-    public String index(){
+    public String index(Model dataToViewFromDB){
         return "index";
+    }
+
+    @GetMapping("/list")
+    public String list(){
+        return "list";
     }
 }
